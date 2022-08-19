b9a3c3ded4204c4ee30eae5874294d2a2024e796
maitatab <mairachinask8@gmail.com>
2022-08-19 11:25:21 -0500

    feat :sparkles: Added login, new registrations and recover password without functionality

---
+++ lib/ui/auth/screens/recover_password.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import '../../../widgets/button_widget.dart';
+import '../../../widgets/input_widget.dart';
+import 'login.dart';
+
+class RecoverPassword extends StatefulWidget {
+  static const route = 'recover-password';
+
+  const RecoverPassword({Key? key}) : super(key: key);
+
+  @override
+  State<RecoverPassword> createState() => _RecoverPasswordState();
+}
+
+class _RecoverPasswordState extends State<RecoverPassword> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  var _isLoading = false;
+
+  // void _submit(BuildContext context) {
+  //   if (_formKey.currentState!.validate()) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     LoginServices(context)
+  //         .recoverPassword(_emailController.text)
+  //         .then((value) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       Navigator.push(
+  //         context,
+  //         MaterialPageRoute(
+  //           builder: (context) => ValidateCodeScreen(
+  //             email: _emailController.text,
+  //           ),
+  //         ),
+  //       );
+  //     }).catchError((error) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     });
+  //   }
+
+  //   return;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SafeArea(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Recuperar Contraseña',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Ingresa tu correo electrónico y nosotros te enviaremos las instrucciones para restablecer tu contraseña.',
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    const SizedBox(height: 20),
+                    Form(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      key: _formKey,
+                      child: InputWidget(
+                        controller: _emailController,
+                        textCapitalization: TextCapitalization.none,
+                        textInputType: TextInputType.emailAddress,
+                        onSubmitted: (val) {},
+                        validate: (val) {
+                          if (val!.trim().isEmpty) {
+                            return 'Introduce tu correo electrónico';
+                          }
+                          return null;
+                        },
+                        hintText: 'Correo Electrónico',
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 60,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                const Color.fromARGB(255, 91, 54, 224)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                    side:
+                                        const BorderSide(color: Colors.blue)))),
+                        child: const Text(
+                          'Enviar correo',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        onPressed: () {
+                          // _login(context);
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    const Text(
+                      '¡No tengo problemas con mi contraseña!',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.normal),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, LoginPage.route);
+                      },
+                      child: const Text('Iniciar Sesión'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/ui/auth/screens/sing_up.dart
@@ -0,0 +1,287 @@
+import 'package:cuivi_medic/ui/auth/screens/login.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../widgets/button_widget.dart';
+import '../../../widgets/input_widget.dart';
+
+class SignupPage extends StatefulWidget {
+  static const route = 'signup';
+
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final FocusNode _confirmPasswordFocusNode = FocusNode();
+  var _isLoading = false;
+
+  // void _submitForm() {
+  //   if (_formKey.currentState!.validate()) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     LoginServices(context)
+  //         .registerUser(
+  //       name: _nameController.text,
+  //       email: _emailController.text,
+  //       password: _passwordController.text,
+  //       passwordConfirmation: _confirmPasswordController.text,
+  //     )
+  //         .then((value) {
+  //       LoginServices(context)
+  //           .requestLogin(
+  //         _emailController.text,
+  //         _passwordController.text,
+  //       )
+  //           .then((value) {
+  //         setState(() {
+  //           _isLoading = false;
+  //         });
+  //         Navigator.pushAndRemoveUntil(
+  //           context,
+  //           PageTransition(
+  //             type: PageTransitionType.bottomToTop,
+  //             child: const HomeScreen(),
+  //           ),
+  //           (Route<dynamic> route) => false,
+  //         );
+  //       });
+  //     }).catchError((error) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     });
+  //   }
+  //   return;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SafeArea(
+              child: SingleChildScrollView(
+                physics: const ClampingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    left: 20.0,
+                    right: 20.0,
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0,
+                          height: size.width * 0.05,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Unete a CUIVI',
+                            style: Theme.of(context).textTheme.headline1,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          'Crea una cuenta y empieza a llevar el control de tu salud al siguiente nivel.',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        const SizedBox(height: 20),
+                        InputWidget(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          textCapitalization: TextCapitalization.words,
+                          controller: _nameController,
+                          onSubmitted: (val) {},
+                          validate: (val) {
+                            if (val!.trim().isEmpty) {
+                              return 'Introduce tu nombre';
+                            }
+                            return null;
+                          },
+                          hintText: 'Nombre',
+                          textInputAction: TextInputAction.next,
+                        ),
+                        const SizedBox(height: 15),
+                        InputWidget(
+                          autocorrect: false,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          textCapitalization: TextCapitalization.none,
+                          controller: _emailController,
+                          onSubmitted: (val) {},
+                          validate: (val) {
+                            if (val!.trim().isEmpty) {
+                              return 'Introduce tu correo electrónico';
+                            }
+                            return null;
+                          },
+                          hintText: 'Correo Electrónico',
+                          textInputAction: TextInputAction.next,
+                        ),
+                        const SizedBox(height: 15),
+                        InputWidget(
+                          controller: _passwordController,
+                          textCapitalization: TextCapitalization.none,
+                          onSubmitted: (val) {},
+                          onComplete: () {
+                            FocusScope.of(context)
+                                .requestFocus(_confirmPasswordFocusNode);
+                          },
+                          validate: (val) {
+                            if (val!.trim().length < 8) {
+                              return 'Mínimo 8 caracteres';
+                            }
+                            if (val != _confirmPasswordController.text) {
+                              return 'Las contraseñas no coinciden';
+                            }
+                            return null;
+                          },
+                          hintText: 'Contraseña',
+                          isPassword: true,
+                          textInputAction: TextInputAction.next,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        const SizedBox(height: 15),
+                        InputWidget(
+                          focus: _confirmPasswordFocusNode,
+                          textCapitalization: TextCapitalization.none,
+                          onSubmitted: (val) {},
+                          controller: _confirmPasswordController,
+                          validate: (val) {
+                            if (val! != _passwordController.text) {
+                              return 'Las contraseñas no coinciden';
+                            }
+                            if (val.trim().length < 8) {
+                              return 'Mínimo 8 caracteres';
+                            }
+                            return null;
+                          },
+                          hintText: 'Confirmar Contraseña',
+                          isPassword: true,
+                          textInputAction: TextInputAction.done,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        const SizedBox(height: 15),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        const Color.fromARGB(255, 91, 54, 224)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0),
+                                        side: const BorderSide(
+                                            color: Colors.blue)))),
+                            child: const Text(
+                              'Registrarme',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                            onPressed: () {
+                              // _login(context);
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('¿Ya tienes una cuenta?'),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                  context,
+                                  LoginPage.route,
+                                );
+                              },
+                              child: const Text('Iniciar Sesión'),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: size.width * 0,
+                          height: size.width * 0.1,
+                        ),
+                        Text.rich(
+                          TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text:
+                                    'Al registrarme estoy de acuerdo y acepto los\n',
+                                style: TextStyle(
+                                  fontSize: size.width * 0.035,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'Términos y condiciones ',
+                                style: TextStyle(
+                                  fontSize: size.width * 0.035,
+                                  color: Colors.redAccent,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    launch(
+                                      'https://pub.dev/packages/url_launcher',
+                                    );
+                                  },
+                              ),
+                              TextSpan(
+                                text: 'y ',
+                                style: TextStyle(
+                                  fontSize: size.width * 0.035,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'Política de privacidad ',
+                                style: TextStyle(
+                                  fontSize: size.width * 0.035,
+                                  color: Colors.redAccent,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    launch(
+                                      'https://pub.dev/packages/url_launcher',
+                                    );
+                                  },
+                              ),
+                              TextSpan(
+                                text: 'de CUIVI.',
+                                style: TextStyle(
+                                  fontSize: size.width * 0.035,
+                                ),
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(height: 1.5),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+}
